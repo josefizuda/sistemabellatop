1352938d0ffb79728e5686c2669f72a0eb141144
josef weslley <josefizuda@hotmail.com>
2019-02-14 11:39:29 -0200

    remover dados incorretos

--- sistema/proc_pesq_ordens (12.02.2019 15.45.30).php
+++ sistema/proc_pesq_ordens (12.02.2019 15.45.30).php
@@ -2,11 +2,9 @@
 //conexao
 include("class/conexao.php");
 $conn = new mysqli($host, $usuario, $senha, $bd);
-
 if(!isset($_SESSION))
                    //se nao existir uma sessao ativa, crie-a
  session_start();
-
 //colunas
 $columns = array(
 'emitir_ordem.emit_id', 
@@ -17,7 +15,6 @@ $columns = array(
 'emitir_ordem.qtyexit',
 'emitir_ordem.qtysaldo', 
 'order_status.status');
-
 //query
 $query = "SELECT emitir_ordem.emit_id, provider.name_pr, order_marcas.marcas, order_size.size, order_sign, emitir_ordem.qtyentry, emitir_ordem.qtyexit, emitir_ordem.qtysaldo, order_status.status FROM emitir_ordem 
             LEFT JOIN user ON user.id_user = emitir_ordem.cod_user
@@ -25,23 +22,12 @@ $query = "SELECT emitir_ordem.emit_id, provider.name_pr, order_marcas.marcas, or
             LEFT JOIN order_marcas ON order_marcas.marcas_id = emitir_ordem.cod_marcas
             LEFT JOIN order_size ON order_size.size_id = emitir_ordem.cod_size
             LEFT JOIN order_status ON order_status.order_status_id = emitir_ordem.cod_order_status WHERE ";
-
-//Obtendo registros de número total sem qualquer pesquisa
-$sql = "SELECT emitir_ordem.emit_id, provider.name_pr, order_marcas.marcas, order_size.size, emitir_ordem.order_sign, emitir_ordem.qtyentry, emitir_ordem.qtyexit, emitir_ordem.qtysaldo, order_status.status FROM emitir_ordem 
-            LEFT JOIN user ON user.id_user = emitir_ordem.cod_user
-            LEFT JOIN provider ON provider.provider_id = emitir_ordem.cod_provider
-            LEFT JOIN order_marcas ON order_marcas.marcas_id = emitir_ordem.cod_marcas
-            LEFT JOIN order_size ON order_size.size_id = emitir_ordem.cod_size
-            LEFT JOIN order_status ON order_status.order_status_id = emitir_ordem.cod_order_status WHERE 1=1";
-
-$resultado_user =mysqli_query($conn,$sql) or trigger_error(mysqli_error($conn));
-$qnt_linhas = mysqli_num_rows($resultado_user);                     
+                     
 //is date
 if($_POST["is_date_search"] == "yes")
 {
  $query .= 'order_sign BETWEEN "'.$_POST["start_date"].'" AND "'.$_POST["end_date"].'" AND ';
 }
-
 if(isset($_POST["search"]["value"]))
 {
  $query .= '
@@ -56,7 +42,6 @@ if(isset($_POST["search"]["value"]))
   OR order_status.status LIKE "%'.$_POST["search"]["value"].'%")
  ';
 }
-
 if(isset($_POST["order"]))
 {
  $query .= 'ORDER BY '.$columns[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' 
@@ -66,22 +51,15 @@ else
 {
  $query .= 'ORDER BY emitir_ordem.emit_id DESC ';
 }
-
 $query1 = '';
-
 if($_POST["length"] != -1)
 {
  $query1 = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
 }
-
 $resultado_usuarios = mysqli_query($conn, $query);
 $totalFiltered   = mysqli_num_rows($resultado_usuarios);
-
 $result = mysqli_query($conn, $query . $query1);
-
 $data = array();
-
- 
    if ($_SESSION['acess_level'] == 2) {
     while($row = mysqli_fetch_array($result))
 {
@@ -96,10 +74,8 @@ $sub_array[] = $row["qtyexit"];
 $sub_array[] = $saldo;
 $sub_array[] = $row["status"];
 $sub_array[]='<a href="editar_ordem.php?id='.$row['emit_id'].'" class="btn btn-success"><i class="glyphicon glyphicon-pencil">&nbsp;</i>BAIXAR</a><br>
-
                 <a href="deletar_ordem.php?p=deletar&id='.$row['emit_id'].'" onclick="return confirm(\'Você tem certeza ?\')" class="btn btn-danger"><i class="glyphicon glyphicon-trash">&nbsp;</i>Deletar</a>';
  $data[] = $sub_array;
-
 }
   }
   else {
@@ -117,15 +93,8 @@ $sub_array[] = $saldo;
 $sub_array[] = $row["status"];
 $sub_array[]='<a href="editar_ordem.php?id='.$row['emit_id'].'" class="btn btn-success"><i class="glyphicon glyphicon-pencil">&nbsp;</i>BAIXAR</a><br>';
  $data[] = $sub_array;
-
 }   
  }
-
-
-
-
-
-
 function get_all_data($conn)
 {
  $query = "SELECT emitir_ordem.emit_id, provider.name_pr, order_marcas.marcas, order_size.size, emitir_ordem.order_sign, emitir_ordem.qtyentry, emitir_ordem.qtyexit, emitir_ordem.qtysaldo, order_status.status FROM emitir_ordem 
@@ -137,14 +106,11 @@ function get_all_data($conn)
  $resultado_usuarios=mysqli_query($conn, $query);
  return mysqli_num_rows($resultado_usuarios);
 }
-
 $output = array(
  "draw"    => intval($_POST["draw"]),
- "recordsTotal"  =>  intval( $qnt_linhas ),
- "recordsFiltered" => intval( $totalFiltered ),
+ "recordsTotal"  =>  get_all_data($conn),
+ "recordsFiltered" => $totalFiltered,
  "data"    => $data
 );
-
 echo json_encode($output);
-
 ?>
